0bb7bd486b3466dbb24aa554ac98875dfbde0732
Han Wen Xie <hanw.xie@gmail.com>
2021-03-16 19:34:38 -0400

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
+const element = <h1>Hello World</h1>;
 ReactDOM.render(
   <React.StrictMode>
     <App />
